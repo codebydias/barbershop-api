a14c786d1d103db067bdf57d5ef0d92d73f41fcc
Vitor <vitormendesesilva1@gmail.com>
2025-09-24 22:36:13 -0300

    feat: adds auth guard, ActiveUserId and ActiveRole to working in private endpoints

---
+++ src/shared/decorators/ActiveRole.ts
@@ -0,0 +1,18 @@
+import {
+  createParamDecorator,
+  ExecutionContext,
+  UnauthorizedException,
+} from "@nestjs/common";
+
+interface RequestWithRole {
+  role: string;
+}
+
+const ActiveRole = createParamDecorator((data, context: ExecutionContext) => {
+  const request = context.switchToHttp().getRequest<RequestWithRole>();
+  const role = request.role;
+
+  if (!role) throw new UnauthorizedException();
+
+  return role;
+});
